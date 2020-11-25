aba7821d0e49eb0068b7c618c5246797c27cdd3a
Matheus da Cruz <matheus.rcruz12@gmail.com>
2020-11-25 20:54:09 -0300

    add scheduling

--- src/main/java/br/com/darstec/integration/IntegrationApplication.java
+++ src/main/java/br/com/darstec/integration/IntegrationApplication.java
@@ -1,16 +1,32 @@
 package br.com.darstec.integration;
 
+import br.com.darstec.integration.domain.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class IntegrationApplication {
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(IntegrationApplication.class, args);
 
 		SchedulingTask scheduling = new SchedulingTask();
 		scheduling.scheduleFutureTask();
 	}
+
+
 
 }
--- src/main/java/br/com/darstec/integration/SchedulingTask.java
+++ src/main/java/br/com/darstec/integration/SchedulingTask.java
@@ -1,19 +1,37 @@
 package br.com.darstec.integration;
 
+import br.com.darstec.integration.domain.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 @EnableScheduling
 public class SchedulingTask {
 
-    private final long MINUT = 1000 * 60 * 10;
+    private final long MINUT = 1000 * 60 * 25;
     private long i = 0;
+    private static final Logger log = LoggerFactory.getLogger(IntegrationApplication.class);
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @Scheduled(fixedDelay = MINUT)
     public void scheduleFutureTask() {
-        System.out.println("schedule ok!" + i);
-        i++;
+        //System.out.println("schedule ok!" + i);
+        if (restTemplate != null){
+            String quote = restTemplate.getForObject(
+                    "https://dars-wake-up.herokuapp.com/hello", String.class);
+            log.info("Resultado da Chamada RestExampleController:    " + quote);
+        }
+
     }
+
 }
